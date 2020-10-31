e97051e129c190d44d6744fae8bb922932926e06
AnusreeK-2000 <AnusreeK-2000>
2020-10-31 19:03:43 +0530

    splash screen update

--- app/src/main/java/com/example/notin/Common/SplashScreen.java
+++ app/src/main/java/com/example/notin/Common/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.notin.Common;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
@@ -26,12 +27,18 @@ public class SplashScreen extends AppCompatActivity {
     //Animations
     Animation sideAnim, bottomAnim;
 
+    SharedPreferences sharedPreferences;
+    Boolean firstTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
 
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        firstTime = sharedPreferences.getBoolean("firstTime", true);
         //Hooks
         backgroundImage = findViewById(R.id.backgroundImage);
         appname = findViewById(R.id.app_name);
@@ -46,14 +53,31 @@ public class SplashScreen extends AppCompatActivity {
         appname.setAnimation(bottomAnim);
         slogan.setAnimation(bottomAnim);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(getApplicationContext(), OnBoarding.class);
-                startActivity(intent);
-                finish();
-            }
-        },SPLASH_TIMER);
+        if(firstTime){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    firstTime = false;
+
+                    editor.putBoolean("firstTime", firstTime);
+                    editor.apply();
+                    Intent intent = new Intent(getApplicationContext(), OnBoarding.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },SPLASH_TIMER);
+
+        }
+        else{
+            Intent intent = new Intent(getApplicationContext(), OnBoarding.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+
 
     }
 }
